2aa7b5eb9c97d43b8c03ba0980c24c88ffa9d298
powerlife145 <powerlife145@gmail.com>
2025-07-01 15:41:59 +0900

    first commit

---
+++ src/Java_2557_1000_1001_10998_1008.java
@@ -0,0 +1,13 @@
+//TIP 코드를 <b>실행</b>하려면 <shortcut actionId="Run"/>을(를) 누르거나
+// 에디터 여백에 있는 <icon src="AllIcons.Actions.Execute"/> 아이콘을 클릭하세요.
+public class Java_2557_1000_1001_10998_1008 {
+    public static void main(String[] args) {
+        //2557번 문제
+        //Hello World!를 출력하시오.
+        System.out.println("Hello World");
+
+        //1000번 문제
+
+
+    }
+}
