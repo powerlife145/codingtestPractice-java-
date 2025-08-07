fa97d81d34e7b0fe21ede3b1e45cb2383244eb38
lee uijin <powerlife145@gmail.com>
2025-08-07 14:23:47 +0900

    baekjoon: 10869_18108
    cat >>
    
    q
    q
    /q
    \q
    # cat

---
+++ src/BaekJoon_10869.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class BaekJoon_10869 {
+
+    public static void main(String[] args) {
+        //백준 10869번 문제
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+
+    }
+}
---
+++ src/BaekJoon_18108.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class BaekJoon_18108 {
+
+    public static void main(String[] args) {
+        //백준 18108번 문제
+        Scanner sc = new Scanner(System.in);
+
+        int ageBuda = sc.nextInt();
+
+        System.out.println(ageBuda-543);
+
+    }
+}
